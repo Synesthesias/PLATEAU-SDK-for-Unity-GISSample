894da9d3333117a54e9c366df35a5a679283572b
linoal <1321932+linoal@users.noreply.github.com>
2024-01-26 23:21:32 +0900

    ゲーム終了ボタンを追加

---
+++ Assets/GISSample/PlateauAttributeDisplay/QuitUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace GISSample.PlateauAttributeDisplay
+{
+    public class QuitUI : MonoBehaviour
+    {
+        private Button quitButton;
+        private void Start()
+        {
+            var ui = GetComponent<UIDocument>();
+            quitButton = ui.rootVisualElement.Q<Button>("quit-app-button");
+            quitButton.clicked += () =>
+            {
+                Debug.Log("Quitting App.");
+                Application.Quit();
+            };
+        }
+    }
+}
