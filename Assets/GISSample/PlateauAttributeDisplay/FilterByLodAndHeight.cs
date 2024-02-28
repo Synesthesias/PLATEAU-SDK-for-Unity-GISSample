e0e122944d4b1e651105ef5e0d4fe3e1fb41ffb7
Linoal <1321932+linoal@users.noreply.github.com>
2024-02-28 17:21:13 +0900

    リファクタ：Dictionary<string, SampleGml>gmlsの処理を別クラスに抽出、MenuUI処理を別クラスに抽出
    同上
    
    同上
    
    uiリファクタ中
    
    リファクタ：MenuUi処理を別クラスに抽出中
    
    MenuUiリファクタ

--- Assets/GISSample/PlateauAttributeDisplay/FilterByLodAndHeight.cs
+++ Assets/GISSample/PlateauAttributeDisplay/FilterByLodAndHeight.cs
@@ -14,59 +14,35 @@ namespace GISSample.PlateauAttributeDisplay
         /// </summary>
         private FilterParameter filterParameter;
 
-        private UIDocument menuUi;
+        private MenuUi menuUi;
         
-        /// <summary>
-        /// 高さフィルターのスライダー
-        /// </summary>
-        private MinMaxSlider heightSlider;
-
-        /// <summary>
-        /// LODフィルターのスライダー
-        /// </summary>
-        private MinMaxSlider lodSlider;
-
-        /// <summary>
-        /// 高さフィルターのラベル
-        /// </summary>
-        private Label heightValueLabel;
-
-        /// <summary>
-        /// LODフィルターのラベル
-        /// </summary>
-        private Label lodValueLabel;
         
-        private readonly Dictionary<string, SampleGml> gmls;
+        private readonly GmlDictionary gmls;
 
-        public FilterByLodAndHeight(UIDocument menuUi, Dictionary<string, SampleGml> gmls)
+        public FilterByLodAndHeight(MenuUi menuUi, GmlDictionary gmls)
         {
             this.menuUi = menuUi;
             this.gmls = gmls;
+
+            menuUi.RegisterHeightSliderChangedCallback(OnHeightSliderValueChanged);
+            menuUi.RegisterLodSliderChangedCallback(OnLodSliderValueChanged);
             
-            heightSlider = menuUi.rootVisualElement.Q<MinMaxSlider>("HeightSlider");
-            heightSlider.RegisterValueChangedCallback(OnHightSliderValueChanged);
-
-            lodSlider = menuUi.rootVisualElement.Q<MinMaxSlider>("LodSlider");
-            lodSlider.RegisterValueChangedCallback(OnLodSliderValueChanged);
-
-            heightValueLabel = menuUi.rootVisualElement.Q<Label>("HeightValue");
-
-            lodValueLabel = menuUi.rootVisualElement.Q<Label>("LodValue");
+            
             
             var param = GetFilterParameterFromSliders();
             Filter(param);
-            UpdateFilterText(param);
+            menuUi.UpdateFilterText(param);
         }
         
         /// <summary>
         /// 高さフィルタースライダーの値変更イベントコールバック
         /// </summary>
         /// <param name="e"></param>
-        public void OnHightSliderValueChanged(ChangeEvent<Vector2> e)
+        public void OnHeightSliderValueChanged(ChangeEvent<Vector2> e)
         {
             filterParameter = GetFilterParameterFromSliders();
             Filter(filterParameter);
-            UpdateFilterText(filterParameter);
+            menuUi.UpdateFilterText(filterParameter);
         }
         
         /// <summary>
@@ -75,11 +51,11 @@ namespace GISSample.PlateauAttributeDisplay
         /// <param name="e"></param>
         public void OnLodSliderValueChanged(ChangeEvent<Vector2> e)
         {
-            lodSlider.value = new Vector2(Mathf.Round(e.newValue.x), Mathf.Round(e.newValue.y));
+            menuUi.lodSlider.value = new Vector2(Mathf.Round(e.newValue.x), Mathf.Round(e.newValue.y));
 
             filterParameter = GetFilterParameterFromSliders();
             Filter(filterParameter);
-            UpdateFilterText(filterParameter);
+            menuUi.UpdateFilterText(filterParameter);
         }
         
         /// <summary>
@@ -88,21 +64,10 @@ namespace GISSample.PlateauAttributeDisplay
         /// <param name="parameter"></param>
         private void Filter(FilterParameter parameter)
         {
-            foreach (var keyValue in gmls)
-            {
-                keyValue.Value.Filter(parameter);
-            }
+            gmls.Filter(parameter);
         }
         
-        /// <summary>
-        /// フィルターのテキストを更新
-        /// </summary>
-        /// <param name="parameter"></param>
-        private void UpdateFilterText(FilterParameter parameter)
-        {
-            heightValueLabel.text = $"{parameter.MinHeight:F1} to {parameter.MaxHeight:F1}";
-            lodValueLabel.text = $"{parameter.MinLod:D} to {parameter.MaxLod:D}";
-        }
+        
         
         /// <summary>
         /// フィルターパラメータを取得
@@ -113,10 +78,10 @@ namespace GISSample.PlateauAttributeDisplay
         {
             return new FilterParameter
             {
-                MinHeight = heightSlider.value.x,
-                MaxHeight = heightSlider.value.y,
-                MinLod = (int)lodSlider.value.x,
-                MaxLod = (int)lodSlider.value.y,
+                MinHeight = menuUi.heightSlider.value.x,
+                MaxHeight = menuUi.heightSlider.value.y,
+                MinLod = (int)menuUi.lodSlider.value.x,
+                MaxLod = (int)menuUi.lodSlider.value.y,
             };
         }
     }
---
+++ Assets/GISSample/PlateauAttributeDisplay/Gml/GmlDictionary.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using GISSample.PlateauAttributeDisplay;
+using PLATEAU.CityInfo;
+using UnityEngine;
+
+/// <summary>
+/// GML辞書
+/// 対象GameObjectやGMLの属性情報等の必要な情報をまとめたものです。
+/// </summary>
+public class GmlDictionary
+{
+    /// <summary>
+    /// キーはGML名です
+    /// </summary>
+    private readonly Dictionary<string, SampleGml> gmls = new Dictionary<string, SampleGml>();
+
+
+    public void Init(PLATEAUInstancedCityModel[] instancedCityModels, SceneManager sceneManager)
+    {
+        foreach (var instancedCityModel in instancedCityModels)
+        {
+
+            for (int i = 0; i < instancedCityModel.transform.childCount; ++i)
+            {
+                // 子オブジェクトの名前はGMLファイル名です。
+                // ロードするときは、一引数に、対応するGameObjectを渡します。
+                var go = instancedCityModel.transform.GetChild(i).gameObject;
+
+                // サンプルではdemを除外します。
+                if (go.name.Contains("dem")) continue;
+                    
+
+                // ロードしたデータをアプリ用に扱いやすくしたクラスに変換します。
+                var gml = new SampleGml(go);
+                if (!gmls.ContainsKey(go.name))
+                {
+                    gmls.Add(go.name, gml);
+                }
+            }
+        }
+
+        var areaNames = new HashSet<string>();
+        foreach(var names in gmls.Select(pair => pair.Value.FloodingAreaNames))
+        {
+            areaNames.UnionWith(names);
+        }
+        sceneManager.floodingAreaNames.AddRange(areaNames);
+        sceneManager.floodingAreaNames.Sort();
+    }
+
+    public void Filter(FilterParameter parameter)
+    {
+        foreach (var keyValue in gmls)
+        {
+            keyValue.Value.Filter(parameter);
+        }
+    }
+
+    public SampleGml GetGml(string gmlName)
+    {
+        return gmls[gmlName];
+    }
+
+    public SampleCityObject GetCityObject(string gmlName, string cityObjName)
+    {
+        return GetGml(gmlName).CityObjects[cityObjName];
+    }
+
+    public SampleAttribute GetAttribute(string gmlFileName, string cityObjectID)
+    {
+        if (gmls.TryGetValue(gmlFileName, out SampleGml gml))
+        {
+            if (gml.CityObjects.TryGetValue(cityObjectID, out SampleCityObject city))
+            {
+                return city.Attribute;
+            }
+        }
+
+        return null;
+    }
+    
+    /// <summary>
+    /// 色分け処理
+    /// </summary>
+    public void ColorCity(ColorCodeType type, string areaName, Color[] heightColorTable, Color[] floodingRankColorTable)
+    {
+        foreach (var keyValue in gmls)
+        {
+            Color[] colorTable = null;
+            switch (type)
+            {
+                case ColorCodeType.Height:
+                    colorTable = heightColorTable;
+                    break;
+                case ColorCodeType.FloodingRank:
+                    colorTable = floodingRankColorTable;
+                    break;
+                default:
+                    break;
+            }
+
+            keyValue.Value.ColorGml(type, colorTable, areaName);
+        }
+    }
+    
+}
