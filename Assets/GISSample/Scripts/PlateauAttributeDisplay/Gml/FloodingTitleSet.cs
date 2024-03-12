420f7fabb49701bf10274228f810103c7446b8c8
Linoal <1321932+linoal@users.noreply.github.com>
2024-03-12 15:22:18 +0900

    建築物と浸水区域で別々に色分けを指定できるようにした
    同上
    
    同上
    
    属性情報色分けUIをColorByAttrUiに抽出
    
    同上
    
    色分けをbldgとfldに分ける準備：UI見た目作成
    
    今まで色分けだったのを建築物と浸水区域で分けて表示
    
    建築物と浸水区域で別々に色分けを指定できるようにした
    
    リファクタ：メソッド整理

---
+++ Assets/GISSample/Scripts/PlateauAttributeDisplay/Gml/FloodingTitleSet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GISSample.PlateauAttributeDisplay.Gml
+{
+    /// <summary>
+    /// 洪水情報のキー（例: ○○川浸水ランク）の集合です。
+    /// </summary>
+    public class FloodingTitleSet
+    {
+        private HashSet<FloodingTitle> data = new();
+
+        public bool Add(FloodingTitle floodingTitle)
+        {
+            return data.Add(floodingTitle);
+        }
+
+        public void UnionWith(FloodingTitleSet other)
+        {
+            data.UnionWith(other.data);
+        }
+
+        public int Count => data.Count;
+
+        /// <summary>
+        /// 集合のキーの一覧を文字列リストで返します。
+        /// </summary>
+        public IEnumerable<string> TitleStrings => data.Select(title => title.ToString());
+
+        /// <summary>
+        /// キー名から検索して返します。
+        /// </summary>
+        public FloodingTitle GetByTitleString(string titleStr)
+        {
+            return data.First(title => title.ToString() == titleStr);
+        }
+    }
+}
